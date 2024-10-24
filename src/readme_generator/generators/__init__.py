5d61835d3bccfdc73d6741c268c87c6a03ce91aa
David Marx <david.marx84@gmail.com>
2024-10-24 12:21:44 -0700

    Update __init__.py

--- src/readme_generator/generators/__init__.py
+++ src/readme_generator/generators/__init__.py
@@ -1,5 +1,5 @@
-from .readme import generate_readme
-from .structure import update_structure
-from .tree import generate_tree
+from .readme_generator import generate_readme
+from .structure_generator import update_structure
+from .tree_generator import generate_tree
 
 __all__ = ['generate_readme', 'update_structure', 'generate_tree']
